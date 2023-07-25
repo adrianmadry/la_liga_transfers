b51dd68ca7df35aa7da58310102869f430ee54d4
Adrian Madry <adrianmadry@gmail.com>
2023-07-25 07:30:32 +0200

    Optimisation of functions

--- stats_from_api.py
+++ stats_from_api.py
@@ -1,135 +1,144 @@
 import requests
 import pandas as pd
 import time
+from typing import Any
+
+# Function that get response from API and handling potential errors
+def get_api_response(url: str, api_headers: dict[str, str], queryparams: dict[str, Any]) -> dict[str, Any]:
+
+    try:
+        response = requests.get(url, headers=api_headers, params=queryparams)
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as errh:
+        print("HTTP Error")
+        print(errh.args[0])
+    except requests.exceptions.ReadTimeout as errt:
+        print("Time out")
+        print(errt.args[0])
+    except requests.exceptions.ConnectionError as errconn:
+        print("Connection error")
+        print(errconn.args[0])
+    except requests.exceptions.MissingSchema as errmiss:
+        print("Missing schema: include http or https")
+        print(errmiss.args[0])
+    except Exception as e:
+        print("An unexpected error occurred:")
+        print(e)
+
+    if response.status_code == 200:
+      return response.json()
+    else:
+      raise Exception(f'Unexpected status code {response.status_code}')
+
+# Function to check total pagination of API response and return list of data from all pages
+def gather_all_pages_data(data: dict[str, Any], url: str, api_headers: dict[str, str], queryparams: dict[str, Any]) -> list[dict]:
+    total_pagination = data['paging']['total']
+
+    if  total_pagination == 1:
+        all_pages_data = data['response']
+
+    else:
+        print(f'No of pages for this season is {total_pagination}')
+        all_pages_data = []
+        # Iterating through all reponse pages
+        for a in range(1, total_pagination + 1):
+            queryparams["page"] = a
+            response = get_api_response(url, api_headers, queryparams)
+            page_data = response['response']
+            print(f'Page number is {a}')
+            print(f'Response lenght is {len(page_data)}')
+            all_pages_data.extend(page_data)
+            print(f'Data List lenght is {len(all_pages_data)}')
+
+            time.sleep(10)
+
+    return all_pages_data
 
 
-# function to get player's statistics data from API
-def stats_for_season(season_id):
-    querystring = {"league": "140", "season": season_id} 
-    response = requests.get(url, headers=headers, params=querystring)
-    data = response.json()
+# Function to get first element from list as dict object
+def get_first_element(record: dict[str, Any], key: str) -> dict[str, dict]:
+  new_dict = {'player': record['player'], key: record[key][0]}
+  return new_dict
 
-    no_of_pages = data['paging']['total']
-    a = 1 
-    season_data = []
-    print('No of pages for this season is', no_of_pages)
 
-    # Iterating through all reponse pages
-    for a in range(1, no_of_pages + 1):
-        querystring = {"league": "140", "season": season_id, "page": a} 
-        response = requests.get(url, headers=headers, params=querystring)
-        page_data = response.json()['response']
-        print('Page number is', a)
-        print('Response lenght is', len(page_data))
-        
-        season_data.extend(page_data)
-        print('Data List lenght is', len(season_data))
-        
-        time.sleep(10)
-          
-    # Make new list with players with more than 1 club in season 
-    more_clubs = []
-    for record in season_data:
-        if len(record['statistics']) > 1:
-            # Make new dict for each club 
-            for stats in record['statistics']:
+
+# --- Function to filter datasets and extract data from JSON file (by given key):
+# 1. If record[key_to_filter] is list with 2 elements or more - create separate dict for each
+# 2. If record[key_to_filter] is list with 1 element - get clean dict object---
+
+def extract_data(dataset: list[dict], endpoint: str) -> list[dict]:
+    # Check if given endpoint is correct and set key_to_filter accordingly
+    if endpoint == 'players':
+        key_to_filter = 'statistics'
+    elif endpoint == 'transfers':
+        key_to_filter = endpoint
+    else:
+        raise ValueError("Invalid endpoint. Only 'players' or 'transfers' are allowed.")
+
+    extracted_list = []
+    for record in dataset:
+        if len(record[key_to_filter]) > 1:
+            # Make new dict for each list element
+            for list_element in record[key_to_filter]:
                 player_name = record['player']
-                player_stats = {'player': player_name, 'statistics': stats}
-                more_clubs.append(player_stats)
-                        
-    # Filter original data by including only players with 1 club in season
-    filtered_data = []
-    for record in season_data:
-        if len(record['statistics']) == 1:
-            filtered_data.append(record)
-            
-    season_data = filtered_data
-            
-    # Get rid of list in season_data statistics key
-    season_data_fixed = []
-    for data in season_data:
-        new_dict = {'player': data['player'], 'statistics': data['statistics'][0]}
-        season_data_fixed.append(new_dict)
-        
-    # Merge data and save it to csv     
-    whole_data = season_data_fixed + more_clubs
-    df = pd.json_normalize(whole_data)
-    df.to_csv(f'./data/stats_from_season_{season_id}.csv', encoding='utf-8-sig')
+                player_data = {'player': player_name, key_to_filter: list_element}
+                extracted_list.append(player_data)
+        elif len(record[key_to_filter]) == 1:
+            new_dict = get_first_element(record, key_to_filter)
+            extracted_list.append(new_dict)
 
-# function to get transfers data from API
-def get_transfers(i):
-    
-    querystring = {"team": i} 
-    response = requests.get(url, headers=headers, params=querystring)
-    data = response.json()['response']
-    
-    # Make new list with players with more than 1 club in season 
-    more_transfers = []
-    for record in data:
-        if len(record['transfers']) > 1:
-            # Make new dict for each club 
-            for transfer in record['transfers']:
-                player_name = record['player']
-                player_transfer = {'player': player_name, 'transfers': transfer}
-                more_transfers.append(player_transfer)
-            
-    # Filter original data by including only players with 1 club in season
-    filtered_data = []
-    for record in data:
-        if len(record['transfers']) == 1:
-            filtered_data.append(record)
-            
-    # Get rid of list in season_data statistics key
-    season_data_fixed = []
-    for data in filtered_data:
-        new_dict = {'player': data['player'], 'transfers': data['transfers'][0]}
-        season_data_fixed.append(new_dict)
-        
-    # Merge data and append it to csv     
-    whole_data = season_data_fixed + more_transfers
-    df = pd.json_normalize(whole_data)
-    
+    return extracted_list
+
+
+# Function to fetch data from API and return DataFrame object
+def fetch_data_from_api(url: str, endpoint: str, api_headers: dict[str, str], queryparams: dict[str, Any]) -> pd.DataFrame:
+    url = url + endpoint 
+    data = get_api_response(url, api_headers, queryparams)
+    all_pages_data = gather_all_pages_data(data, url, api_headers, queryparams)
+    extracted_data = extract_data(all_pages_data, endpoint)   
+    df = pd.json_normalize(extracted_data)
     return df
+
+# Function to append pd.DataFrame to CSV file
+def append_dataframe_to_csv(data_frame: pd.DataFrame, get_headers: bool) -> None:
+    with open('./data/transfers_from_api.csv', 'a+', newline='', encoding='utf-8-sig') as file:
+                data_frame.to_csv(file, header=get_headers)
+
+
+# Function to retrieve players statistics for specified league and specified season range
+# Function output is separate CSV file for each season
+def get_players_stats(url: str, api_key:str, start_season: int, end_season: int, league_id: int) -> None:
+    headers = {"x-rapidapi-host": "v3.football.api-sports.io", "x-rapidapi-key": api_key}
+
+    for season_id in range(start_season, end_season + 1):        
+        df = fetch_data_from_api(url=url, endpoint='players', api_headers=headers, queryparams={"league": league_id, "season": season_id})
+        df.to_csv(f'./data/stats_from_season_{season_id}.csv', encoding='utf-8-sig')
         
 
-# Connect with API
-url = "https://v3.football.api-sports.io/transfers"
+# Function to fetch transfers data from given teams IDs
+# Output is one CSV file with transfer data for all teams
+def get_transfers_data(url: str, api_key:str, teams_ids: list[range | int]) -> None:
+  headers = {"x-rapidapi-host": "v3.football.api-sports.io", "x-rapidapi-key": api_key}
+  
+  csv_headers = True
+  for list_elem in teams_ids:
+    if isinstance(list_elem, range):
+      # Iterate over range
+      for item in list_elem:
+        df = fetch_data_from_api(url, 'transfers', api_headers=headers, queryparams={"team": item})
+        append_dataframe_to_csv(data_frame=df, get_headers=csv_headers)
+        csv_headers = False # No headers should be appended to CSV after 1st team data append
 
-headers = {
-	"x-rapidapi-host": "v3.football.api-sports.io",
-	"x-rapidapi-key": "b69bc50a7b3f69db6443565654f106fc" # Insert user API key
-}
+    elif isinstance(list_elem, int):
+      df = fetch_data_from_api(url, 'transfers', api_headers=headers, queryparams={"team": list_elem})
+      append_dataframe_to_csv(data_frame=df, get_headers=csv_headers)
+      csv_headers = False # No headers should be appended to CSV after 1st team data append
 
-# Get statistics data and add to CSV       
-for season_id in range(2015, 2023):
-    stats_for_season(season_id)
-    
+    else:
+      raise ValueError('teams_ids parameter should be list containing only RANGE or INT data types !')
 
-# Get transfer data for each team
-    # Teams IDs to get data 
-teams_ids = [(529, 549), (711, 757), (797, 800)]
 
-    # Create new CSV file      
-with open('./data/transfers_from_api.csv', 'w', newline='', encoding='utf-8-sig') as f:
-    pass
 
-    # Get data from API and append to CSV    
-a = 0 # flag for ommit headers  
-for start, end in teams_ids:
-    for i in range(start, end):
-        df = get_transfers(i)
-        
-        if a == 0: # get headers in 1st iteration
-            with open('./data/transfers_from_api.csv', 'a+', newline='', encoding='utf-8-sig') as file:
-                df.to_csv(file, header=True)
-            a += 1
-            # print(f'I have aded 1st line, id = {i}')
-        
-        else: # rest data without headers
-            with open('./data/transfers_from_api.csv', 'a+', newline='', encoding='utf-8-sig') as file:
-                df.to_csv(file, header=False)
-            # print(f'I have added another one, id = {i}')
-        time.sleep(5)
-   
-       
+# get_transfers_data(url="https://v3.football.api-sports.io/", api_key="xxxxxxx", teams_ids=[755, range(756, 760)])
+# get_players_stats(url="https://v3.football.api-sports.io/", api_key="xxxxxxx", start_season=2015, end_season=2016, league_id=140) 
   
