5689f2e39a0a9e18cf53b5f6a3c5f74355b5a137
Adrian Madry <adrianmadry@gmail.com>
2023-08-29 21:23:07 +0200

    Add test file for stats_from_api

--- stats_from_api.py
+++ stats_from_api.py
@@ -10,25 +10,21 @@ def get_api_response(url: str, api_headers: dict[str, str], queryparams: dict[st
         response = requests.get(url, headers=api_headers, params=queryparams)
         response.raise_for_status()
     except requests.exceptions.HTTPError as errh:
-        print("HTTP Error")
-        print(errh.args[0])
+        raise errh
     except requests.exceptions.ReadTimeout as errt:
-        print("Time out")
-        print(errt.args[0])
-    except requests.exceptions.ConnectionError as errconn:
-        print("Connection error")
-        print(errconn.args[0])
+        raise errt
+    except requests.exceptions.ConnectionError as errc:
+        raise errc
     except requests.exceptions.MissingSchema as errmiss:
-        print("Missing schema: include http or https")
-        print(errmiss.args[0])
-    except Exception as e:
-        print("An unexpected error occurred:")
-        print(e)
+        raise errmiss
+    except requests.exceptions.RequestException as errexc:
+        raise errexc
 
-    if response.status_code == 200:
+    if response.status_code in range (200, 299):
       return response.json()
     else:
       raise Exception(f'Unexpected status code {response.status_code}')
+
 
 # Function to check total pagination of API response and return list of data from all pages
 def gather_all_pages_data(data: dict[str, Any], url: str, api_headers: dict[str, str], queryparams: dict[str, Any]) -> list[dict]:
@@ -38,57 +34,48 @@ def gather_all_pages_data(data: dict[str, Any], url: str, api_headers: dict[str,
         all_pages_data = data['response']
 
     else:
-        print(f'No of pages for this season is {total_pagination}')
+        # print(f'No of pages for this season is {total_pagination}')
         all_pages_data = []
-        # Iterating through all reponse pages
-        for a in range(1, total_pagination + 1):
-            queryparams["page"] = a
+        for page_num in range(1, total_pagination + 1): # Iterating through all reponse pages
+            queryparams["page"] = page_num # add to query no of page from get data
             response = get_api_response(url, api_headers, queryparams)
             page_data = response['response']
-            print(f'Page number is {a}')
-            print(f'Response lenght is {len(page_data)}')
+            # print(f'Page number is {page_num}')
+            # print(f'Response lenght is {len(page_data)}')
             all_pages_data.extend(page_data)
-            print(f'Data List lenght is {len(all_pages_data)}')
+            # print(f'Data List lenght is {len(all_pages_data)}')
 
             time.sleep(10)
 
     return all_pages_data
 
 
-# Function to get first element from list as dict object
-def get_first_element(record: dict[str, Any], key: str) -> dict[str, dict]:
-  new_dict = {'player': record['player'], key: record[key][0]}
-  return new_dict
+# Function that verify if given endpoint is correct and return key to filter data
+def endpoint_validation(endpoint: str) -> str:
+  if endpoint == 'players':
+      return 'statistics'
+  elif endpoint == 'transfers':
+      return endpoint
+  else:
+      raise ValueError("Invalid endpoint. Only 'players' or 'transfers' are allowed.")
 
-
-
-# --- Function to filter datasets and extract data from JSON file (by given key):
-# 1. If record[key_to_filter] is list with 2 elements or more - create separate dict for each
-# 2. If record[key_to_filter] is list with 1 element - get clean dict object---
-
+# Function to extract and organize data
+# Function transform input data by filtering based on given endpoint ('players' or 'transfers')
+# It separates events, such as transfers or statistics, from nested dictionaries into individual event dictionaries
 def extract_data(dataset: list[dict], endpoint: str) -> list[dict]:
-    # Check if given endpoint is correct and set key_to_filter accordingly
-    if endpoint == 'players':
-        key_to_filter = 'statistics'
-    elif endpoint == 'transfers':
-        key_to_filter = endpoint
-    else:
-        raise ValueError("Invalid endpoint. Only 'players' or 'transfers' are allowed.")
+  # Check if given endpoint is correct and set key_to_filter accordingly
+  key_to_filter = endpoint_validation(endpoint)
 
-    extracted_list = []
-    for record in dataset:
-        if len(record[key_to_filter]) > 1:
-            # Make new dict for each list element
-            for list_element in record[key_to_filter]:
-                player_name = record['player']
-                player_data = {'player': player_name, key_to_filter: list_element}
-                extracted_list.append(player_data)
-        elif len(record[key_to_filter]) == 1:
-            new_dict = get_first_element(record, key_to_filter)
-            extracted_list.append(new_dict)
+  extracted_list = []
+  for record in dataset:
+      # Iterate through each 'list_elem' in the selected key (either 'statistics' or 'transfers')
+      for list_elem in record[key_to_filter]:
+          # Create a new dictionary for each 'list_elem' 
+          new_dict = {'player': record['player'], key_to_filter: list_elem}
+          extracted_list.append(new_dict)
 
-    return extracted_list
-
+  return extracted_list
+    
 
 # Function to fetch data from API and return DataFrame object
 def fetch_data_from_api(url: str, endpoint: str, api_headers: dict[str, str], queryparams: dict[str, Any]) -> pd.DataFrame:
@@ -98,11 +85,6 @@ def fetch_data_from_api(url: str, endpoint: str, api_headers: dict[str, str], qu
     extracted_data = extract_data(all_pages_data, endpoint)   
     df = pd.json_normalize(extracted_data)
     return df
-
-# Function to append pd.DataFrame to CSV file
-def append_dataframe_to_csv(data_frame: pd.DataFrame, get_headers: bool) -> None:
-    with open('./data/transfers_from_api.csv', 'a+', newline='', encoding='utf-8-sig') as file:
-                data_frame.to_csv(file, header=get_headers)
 
 
 # Function to retrieve players statistics for specified league and specified season range
@@ -115,28 +97,34 @@ def get_players_stats(url: str, api_key:str, start_season: int, end_season: int,
         df.to_csv(f'./data/stats_from_season_{season_id}.csv', encoding='utf-8-sig')
         
 
-# Function to fetch transfers data from given teams IDs
-# Output is one CSV file with transfer data for all teams
+def list_of_ints(list_to_handle: list[range | int]) -> list[int]:
+    list_of_teams_ids = []
+    for list_elem in list_to_handle:
+        if isinstance(list_elem, range): # Get sep INTs from range
+            for item in list_elem:
+                list_of_teams_ids.append(item)
+
+        elif isinstance(list_elem, int):
+            list_of_teams_ids.append(list_elem)
+
+        else:
+            raise ValueError("""teams_ids parameter should be list containing only RANGE or INT data types !
+            For range please use syntax 'range(int,int)'
+                                """) 
+    return list_of_teams_ids
+
+
 def get_transfers_data(url: str, api_key:str, teams_ids: list[range | int]) -> None:
-  headers = {"x-rapidapi-host": "v3.football.api-sports.io", "x-rapidapi-key": api_key}
-  
-  csv_headers = True
-  for list_elem in teams_ids:
-    if isinstance(list_elem, range):
-      # Iterate over range
-      for item in list_elem:
-        df = fetch_data_from_api(url, 'transfers', api_headers=headers, queryparams={"team": item})
-        append_dataframe_to_csv(data_frame=df, get_headers=csv_headers)
-        csv_headers = False # No headers should be appended to CSV after 1st team data append
-
-    elif isinstance(list_elem, int):
-      df = fetch_data_from_api(url, 'transfers', api_headers=headers, queryparams={"team": list_elem})
-      append_dataframe_to_csv(data_frame=df, get_headers=csv_headers)
-      csv_headers = False # No headers should be appended to CSV after 1st team data append
-
-    else:
-      raise ValueError('teams_ids parameter should be list containing only RANGE or INT data types !')
-
+    headers = {"x-rapidapi-host": "v3.football.api-sports.io", "x-rapidapi-key": api_key}
+    csv_headers = True # Parameter to controll writeting headers to CSV
+    list_of_teams_ids = list_of_ints(teams_ids)
+    
+    # Get data for each team ID and save it all to one CSV file  
+    for team_id in list_of_teams_ids: 
+        df = fetch_data_from_api(url, 'transfers', api_headers=headers, queryparams={"team": team_id})
+        with open('./data/transfers_from_api.csv', 'a+', newline='', encoding='utf-8-sig') as file:
+                df.to_csv(file, header=csv_headers)
+        csv_headers = False # No headers should be appended to CSV after get 1st team data
 
 
 # get_transfers_data(url="https://v3.football.api-sports.io/", api_key="xxxxxxx", teams_ids=[755, range(756, 760)])
---
+++ tests/tests_stats_from_api.py
@@ -0,0 +1,347 @@
+from stats_from_api import endpoint_validation, extract_data, get_api_response, gather_all_pages_data, fetch_data_from_api, get_players_stats, get_transfers_data, list_of_ints
+import unittest
+from unittest.mock import patch, Mock, mock_open
+from requests.exceptions import HTTPError, ReadTimeout, ConnectionError, MissingSchema, RequestException
+import pandas as pd
+
+
+class TestEndpointValidation(unittest.TestCase):
+    def test_players_endpoint(self):
+        endpoint = 'players'
+        result = endpoint_validation(endpoint)
+        expected_result = 'statistics'
+        
+        self.assertEqual(result, expected_result)
+
+    def test_transfers_endpoint(self):
+        endpoint = 'transfers'
+        result = endpoint_validation(endpoint)
+        expected_result = endpoint
+        
+        self.assertEqual(result, expected_result)
+        
+    def test_invalid_endoint(self):
+        endpoint = 'something'
+        
+        with self.assertRaises(ValueError):
+            endpoint_validation(endpoint)
+
+class TestExtractData(unittest.TestCase):
+    # Test when there's only one element inside the key(endpoint) list (literally func have to only remove list from record['transfers'] value)
+    def test_one_elem_inside_key_list(self):
+        dataset = [
+            {'player': {'id': 25, 'name': 'Kowalski'},
+             'transfers': [{'date': '2000-03-03', 'type': 'Free'}]},
+             {'player': {'id': 8, 'name': 'Majski'},
+             'transfers': [{'date': '2000-05-05', 'type': 'Free'}]}    
+        ]
+        endpoint = 'transfers'
+        
+        result = extract_data(dataset, endpoint)
+        
+        expected_result = [
+            {'player': {'id': 25, 'name': 'Kowalski'},
+             'transfers': {'date': '2000-03-03', 'type': 'Free'}},
+            {'player': {'id': 8, 'name': 'Majski'},
+             'transfers': {'date': '2000-05-05', 'type': 'Free'}}   
+        ]
+        
+        self.assertEqual(result, expected_result)
+    
+    # Test when there are multiple elements inside the key list (create a new dict for eac )    
+    def test_more_elem_inside_key_list(self):
+        dataset = [
+            {'player': {'id': 25, 'name': 'Kowalski'},
+             'transfers': [{'date': '2000-03-03', 'type': 'Free'},
+                           {'date': '2001-04-04', 'type': '2M'}]},
+             {'player': {'id': 8, 'name': 'Majski'},
+             'transfers': [{'date': '2000-05-05', 'type': 'Free'},
+                           {'date': '2002-05-05', 'type': '2K'}]}    
+        ]
+        endpoint = 'transfers'
+        result = extract_data(dataset, endpoint)
+        expected_result = [
+            {'player': {'id': 25, 'name': 'Kowalski'},
+             'transfers': {'date': '2000-03-03', 'type': 'Free'}},
+            {'player': {'id': 25, 'name': 'Kowalski'},
+             'transfers': {'date': '2001-04-04', 'type': '2M'}},
+            {'player': {'id': 8, 'name': 'Majski'},
+             'transfers': {'date': '2000-05-05', 'type': 'Free'}},
+            {'player': {'id': 8, 'name': 'Majski'},
+             'transfers': {'date': '2002-05-05', 'type': '2K'}}    
+        ]
+        
+        self.assertEqual(result, expected_result)
+        
+    # Test when th key list is empty  
+    def test_empty_key_list(self):
+        dataset = [
+            {'player': {'id': 25, 'name': 'Kowalski'},
+             'transfers': [{'date': '2000-03-03', 'type': 'Free'},
+                           {'date': '2001-04-04', 'type': '2M'}]},
+             {'player': {'id': 8, 'name': 'Majski'},
+             'transfers': []}    
+        ]
+        endpoint = 'transfers'
+        result = extract_data(dataset, endpoint)
+        expected_result = [
+            {'player': {'id': 25, 'name': 'Kowalski'},
+             'transfers': {'date': '2000-03-03', 'type': 'Free'}},
+            {'player': {'id': 25, 'name': 'Kowalski'},
+             'transfers': {'date': '2001-04-04', 'type': '2M'}}      
+        ]
+        
+        self.assertEqual(result, expected_result)
+    
+class TestGetApiResponse(unittest.TestCase):
+
+    @patch('stats_from_api.requests.get')
+    def test_positive_response(self, mock_get):
+        mock_response = Mock()
+        mock_response.status_code = 200
+        mock_response.json.return_value = {'foo': {'abc': 'xyz'}}
+        
+        mock_get.return_value = mock_response
+        result = get_api_response('http://example.com/api', {'user': 'key'}, {'param': 'value'})
+        self.assertEqual(result, mock_response.json.return_value)
+
+    @patch('stats_from_api.requests.get')
+    def test_http_err(self, mock_get): 
+        # Simulate HTTP error during requests.get
+        mock_get.side_effect = HTTPError("404 Client Error: Not Found for url: http://example.com/api")
+        
+        with self.assertRaises(HTTPError) as context:
+            get_api_response('http://example.com/api', {}, {})
+
+        self.assertEqual(str(context.exception), "404 Client Error: Not Found for url: http://example.com/api")
+        
+    @patch('stats_from_api.requests.get')
+    def test_timeout_err(self, mock_get):
+        # Simulate timeout error during requests.get
+        mock_get.side_effect = ReadTimeout("Connection timed out")
+        
+        with self.assertRaises(ReadTimeout) as context:
+            get_api_response('http://example.com/api', {}, {})
+
+        self.assertEqual(str(context.exception), "Connection timed out")
+        
+    @patch('stats_from_api.requests.get')
+    def test_connection_err(self, mock_get):
+        # Simulate connection error during requests.get
+        mock_get.side_effect = ConnectionError("Connection error occurred")
+        
+        with self.assertRaises(ConnectionError) as context:
+            get_api_response('http://example.com/api', {}, {})
+
+        self.assertEqual(str(context.exception), "Connection error occurred")
+        
+    
+    @patch('stats_from_api.requests.get')
+    def test_miss_schema_err(self, mock_get):
+        # Simulate missing schema error during requests.get
+        mock_get.side_effect = MissingSchema("Invalid URL 'example.com/api': No scheme supplied. Perhaps you meant http://example.com/api?")
+        
+        with self.assertRaises(MissingSchema) as context:
+            get_api_response('example.com/api', {}, {})
+
+        self.assertEqual(str(context.exception), "Invalid URL 'example.com/api': No scheme supplied. Perhaps you meant http://example.com/api?")
+           
+    @patch('stats_from_api.requests.get')
+    def test_req_exc_err(self, mock_get):
+        # Simulate missing schema error during requests.get
+        mock_get.side_effect = RequestException("An unexpected error occurred:")
+        
+        with self.assertRaises(RequestException) as context:
+            get_api_response('example.com/api', {}, {})
+
+        self.assertEqual(str(context.exception), "An unexpected error occurred:")
+        
+    @patch('stats_from_api.requests.get')
+    def test_exception_err(self, mock_get):
+        # Create mock object (response)
+        mock_response = Mock()
+        mock_response.status_code = 400
+        
+        mock_get.return_value = mock_response
+        
+        with self.assertRaises(Exception) as context:
+            get_api_response('http://example.com/api', {}, {})
+        
+        self.assertEqual(str(context.exception), "Unexpected status code 400")
+
+class TestGatherAllPagesData(unittest.TestCase):
+    
+    def test_single_page(self):
+        
+        data = {"get": "endpoint",
+                "parameters": {'sth': 1},
+                "paging": {'current': 1, 'total': 1}, 
+                "response": [{"foo": 1}, {"xyz": 2}]
+                }
+        url = ''
+        api_headers = {}
+        queryparams={}
+        
+        result = gather_all_pages_data(data, url, api_headers, queryparams)
+        expected_result = [{"foo": 1}, {"xyz": 2}]
+      
+        self.assertEqual(result, expected_result)
+
+    @patch('stats_from_api.get_api_response')
+    @patch('time.sleep', Mock())
+    def test_more_pages(self, mock_api_resp):
+        
+        data = {"get": "endpoint",
+                "parameters": {'sth': 1},
+                "paging": {'current': 1, 'total': 3}, 
+                "response": [{"foo": 1}, {"xyz": 2}]
+                }
+        url = ''
+        api_headers = {}
+        queryparams={}
+        
+        # Define mock responses for each page
+        mock_responses = [
+        {"paging": {'current': 1, 'total': 3},
+         'response': [{"k1": "data_value_page_1"}]},
+        {"paging": {'current': 2, 'total': 3},
+         'response': [{"k2": "data_value_page_2"}]},
+        {"paging": {'current': 3, 'total': 3},
+         'response': [{"k3": "data_value_page_3"}]}
+        ]
+        
+        # Set side effect to return diif response for each call
+        mock_api_resp.side_effect = mock_responses
+        
+        result = gather_all_pages_data(data, url, api_headers, queryparams)
+        
+        expected_result = [{"k1": "data_value_page_1"},
+                           {"k2": "data_value_page_2"},
+                           {"k3": "data_value_page_3"}]
+      
+        self.assertEqual(result, expected_result)
+        
+    def test_empty_response(self):
+        
+        data = {"get": "endpoint",
+                "parameters": {'sth': 1},
+                "paging": {'current': 1, 'total': 1}, 
+                "response": []
+                }
+        url = ''
+        api_headers = {}
+        queryparams={}
+        
+        result = gather_all_pages_data(data, url, api_headers, queryparams)
+        expected_result = []
+      
+        self.assertEqual(result, expected_result)
+    
+class TestFetchDataFromApi(unittest.TestCase):
+    
+    @patch('stats_from_api.get_api_response')
+    @patch('stats_from_api.gather_all_pages_data')
+    @patch('stats_from_api.extract_data')
+    def test_fetch_data_from_api(self, mock_extract, mock_gather, mock_get):
+        url = ''
+        endpoint = 'endpoint'
+        api_headers = {}
+        queryparams={}
+        data = {"get": "some",
+                "parameters": {'sth': 1},
+                "paging": {'current': 1, 'total': 1}, 
+                "response": [{"foo": 1}, {"xyz": 2}]
+                }
+        all_pages_data = [{"foo": 1}, {"xyz": 2}]
+        extracted_data = [{"foo": 1}, {"xyz": 2}]
+        
+        mock_get.return_value = data
+        mock_gather.return_value = all_pages_data
+        mock_extract.return_value = extracted_data
+        
+        result_df = fetch_data_from_api(url, endpoint, api_headers, queryparams)
+        expected_df = pd.json_normalize(extracted_data)
+        
+        
+        pd.testing.assert_frame_equal(result_df, expected_df)
+        
+class TestGetPlayerStats(unittest.TestCase):
+    
+    @patch('stats_from_api.fetch_data_from_api')
+    @patch('pandas.DataFrame.to_csv')
+    def test_get_players_stats(self, mock_to_csv, mock_fetch):
+        url = ''
+        api_key = 'key'
+        start_season = 2018
+        end_season = 2020
+        league_id = 1
+        
+        mock_df = pd.DataFrame({
+            'Name': ['Alan', 'John', 'Leo'],
+            'Club': ['NYC', 'LA', 'SA'],
+            'Score': [2.5, 5.1, 3.8]
+        })
+           
+        mock_fetch.return_value = mock_df
+        
+        get_players_stats(url, api_key, start_season, end_season, league_id)
+        
+        expected_calls_to_csv = [
+            unittest.mock.call(f'./data/stats_from_season_2018.csv', encoding='utf-8-sig'),
+            unittest.mock.call(f'./data/stats_from_season_2019.csv', encoding='utf-8-sig'),
+            unittest.mock.call(f'./data/stats_from_season_2020.csv', encoding='utf-8-sig')
+        ]
+        
+        mock_to_csv.assert_has_calls(expected_calls_to_csv)
+        
+        
+    @patch('stats_from_api.fetch_data_from_api')
+    @patch('pandas.DataFrame.to_csv')
+    def test_get_players_stats_csv_err(self, mock_to_csv, mock_fetch):
+        url = ''
+        api_key = 'key'
+        start_season = 2018
+        end_season = 2020
+        league_id = 1
+        
+        mock_df = pd.DataFrame({
+            'Name': ['Alan', 'John', 'Leo'],
+            'Club': ['NYC', 'LA', 'SA'],
+            'Score': [2.5, 5.1, 3.8]
+        })
+           
+        mock_fetch.return_value = mock_df
+        
+        mock_to_csv.side_effect = Exception("CSV writing error")
+        with self.assertRaises(Exception):
+            get_players_stats(url, api_key, start_season, end_season, league_id)
+            
+        mock_to_csv.assert_called()
+    
+class TestGettransfersData(unittest.TestCase):
+    
+    # --- Test if 'open' clause has been called correct number of times ---
+    @patch('stats_from_api.fetch_data_from_api')
+    @patch('stats_from_api.list_of_ints')
+    def test_get_transfer_data(self, mock_list_of_ints, mock_fetch):
+        url = ''
+        api_key = 'key'
+        teams_ids = [1, 2, range(5,7), 25]
+        
+        mock_df = pd.DataFrame({
+            'Name': ['Alan', 'John', 'Leo'],
+            'Club': ['NYC', 'LA', 'SA'],
+            'Score': [2.5, 5.1, 3.8]
+        })
+        mock_fetch.return_value = mock_df
+        mock_list_of_ints.return_value = [1, 2, 5, 6, 25]
+        
+        with patch('builtins.open', mock_open()) as mock_open_file:
+            get_transfers_data(url, api_key, teams_ids)
+            
+            self.assertEqual(mock_open_file.call_count, 5)
+            
+            
+              
+if __name__ == "__main__":
+    unittest.main()
